3af07ef2ae67edae5b8db6b4efaaaa0317e72565
Tom Ritchford <tom@swirly.com>
2023-04-29 15:51:54 +0200

    Refactor sketch

--- fil.py
+++ fil.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from typing import Dict, Iterator, List, Union
+from typing import Dict, Iterator, List, Optional, Union
 import json
 import safer
 
@@ -17,9 +17,15 @@ def read(path: FilePath) -> FileData:
     return _get_class(p).read(p)
 
 
-def write(data: FileData, path: FilePath, **kwargs: Dict) -> None:
+def write(
+    data: FileData,
+    path: FilePath,
+    *,
+    use_safer: Optional[bool] = None,
+    **kwargs: Dict,
+) -> None:
     p = Path(path)
-    return _get_class(p).write(data, p, **kwargs)
+    return _get_class(p).write(data, p, use_safer=use_safer, **kwargs)
 
 
 def _get_class(p):
@@ -29,10 +35,10 @@ def _get_class(p):
         raise ValueError('Do not understand file {p}')
 
 
-class _Base:
-    suffixes = ()
+class _Json:
+    module_names = 'json',
+    suffixes = '.json',
     use_safer = True
-    module_names = ()
 
     def __init__(self):
         for m in self.module_names:
@@ -43,28 +49,23 @@ class _Base:
                 pass
         self._import_error()
 
+    def read(self, p):
+        with open(p) as fp:
+            return self._read(fp)
+
+    def write(self, data, p, use_safer=None, **kwargs):
+        if use_safer or use_safer is None and self.use_safer:
+            fp = safer.open(p, 'w')
+        else:
+            fp = open(p, 'w')
+
+        with fp:
+            self._write(data, fp, **kwargs)
+
     def _import_error(self):
         name = self.module_name
         sfx = ', '.join(self.suffixes)
         raise ImportError(f'Install module `{name}` is needed for {sfx} files')
-
-    def read(self, p):
-        with p.open() as fp:
-            return self._read(fp)
-
-    def write(self, data, p, *, **kwargs):
-        use_safer = kwargs.pop('use_safer', None)
-        if use_safer is None:
-            use_safer = self.use_safer:
-        fp = safer.open(p) if use_safer else open(p)
-        with fp:
-            self._write(data, fp, **kwargs)
-
-    def _open(self, p, kwargs):
-        use_safer = kwargs.pop('use_safer', None)
-        if use_safer is None:
-            use_safer = self.use_safer:
-        return safer.open(p) if use_safer else open(p)
 
     @property
     def _read(self):
@@ -72,27 +73,18 @@ class _Base:
 
     @property
     def _write(self):
-        return self._module.dump
-
-
-class _Json(_Base):
-    suffixes = '.json',
-    module_names = 'json',
-
-
-class _Toml(_Base):
-    suffixes = '.toml',
-    module_names = 'tomlkit',
-
-    @property
-    def _write(self):
         try:
-            return super()._write
+            return self._module.dump
         except AttributeError:
             self._import_error()
 
 
-class _Yaml(_Base):
+class _Toml(_Json):
+    suffixes = '.toml',
+    module_names = 'tomlkit',
+
+
+class _Yaml(_Json):
     suffixes = '.yaml', '.yml'
     module_names = 'pyyaml',
 
@@ -105,15 +97,15 @@ class _Yaml(_Base):
         return self._module.safe_dump
 
 
-class _JsonLines:
+class _JsonLines(_Json):
     use_safer = False
     suffixes = '.jl', '.jsonl', '.jsonlines'
 
-    def _read(self, p):
+    def _read(self, fp):
         for line in fp:
             yield json.loads(line)
 
-    def _write(self, data, p, *, **kwargs):
+    def _write(self, data, fp, **kwargs):
         if kwargs.get('indent') is not None:
             raise ValueError('indent= not allowed for JSON Lines')
 
@@ -121,84 +113,5 @@ class _JsonLines:
             print(json.dumps(d), file=fp)
 
 
-class _Json:
-    @staticmethod
-    def read(p):
-        with p.open() as fp:
-            return json.load(fp)
-
-    @staticmethod
-    def write(data, p, *, use_safer=True, **kwargs):
-        with _open(p, use_safer) as fp:
-            json.dump(data, fp, **kwargs)
-
-
-class _Yaml:
-    @staticmethod
-    def read(p):
-        with p.open() as fp:
-            return _yaml().safe_load(fp)
-
-    @staticmethod
-    def write(data, p, *, use_safer=True, **kwargs):
-        with _open(p, use_safer) as fp:
-            _yaml().safe_dump(data, fp, **kwargs)
-
-
-def _yaml():
-    try:
-        import pyyaml
-        return pyyaml
-    except ImportError:
-        raise ImportError('Install `pyyaml` to handle .yaml files')
-
-
-class _Toml:
-    @staticmethod
-    def read(p):
-        try:
-            import tomllib as toml
-        except ImportError:
-            toml = _toml()
-        return toml.loads(p.read_text())
-
-    @staticmethod
-    def write(data, p, *, use_safer=True, **kwargs):
-        with _open(p, use_safer) as fp:
-            _toml().dump(data, fp, **kwargs)
-
-
-def _toml():
-    try:
-        import tomlkit
-        return tomlkit
-    except ImportError:
-        raise ImportError('Install `tomlkit` to handle .toml files')
-
-
-class _JsonLines:
-    @staticmethod
-    def read(p):
-        with p.open() as fp:
-            for line in fp:
-                yield json.loads(line)
-
-    @staticmethod
-    def write(data, p, *, use_safer=False, **kwargs):
-        if kwargs.get('indent') is not None:
-            raise ValueError('indent= not allowed for JSON Lines')
-
-        with _open(p, use_safer) as fp:
-            for d in data:
-                print(json.dumps(d), file=fp)
-
-
-SUFFIX_TO_CLASS = {
-    '.jl': _JsonLines,
-    '.json': _Json,
-    '.jsonl': _JsonLines,
-    '.jsonline': _JsonLines,
-    '.toml': _Toml,
-    '.yaml': _Yaml,
-    '.yml': _Yaml,
-}
+CLASSES = _Json, _JsonLines, _Toml, _Yaml
+SUFFIX_TO_CLASS = {s: c for c in CLASSES for s in c.suffixes}
