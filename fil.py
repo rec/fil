2a43f0040729e74b5a109a711e6316c536149bad
Tom Ritchford <tom@swirly.com>
2023-04-29 12:44:10 +0200

    Fill out code

--- fil.py
+++ fil.py
@@ -3,55 +3,76 @@ from typing import Dict, Iterator, List, Union
 import json
 import safer
 
+__all__ = 'read', 'write', 'SUFFIX_TO_CLASS'
+
 # JSON is the type used by JSON, TOML, or Yaml
 JSON = Union[Dict, List, None, bool, float, int, str]
 JSON_Lines = Iterator[JSON]
 FileData = Union[JSON, JSON_Lines]
 FilePath = Union[Path, str]
-JSON_LINE_SUFFIXES = '.jl', '.jsonl', '.jsonline'
-# _NONE = object()
 
 
 def read(path: FilePath) -> FileData:
     p = Path(path)
+    return _get_class(p).read(p)
 
-    if p.suffix == '.json':
+
+def write(data: FileData, path: FilePath, **kwargs: Dict) -> None:
+    p = Path(path)
+    return _get_class(p).write(data, p, **kwargs)
+
+
+def _get_class(p):
+    try:
+        return SUFFIX_TO_CLASS[p.suffix]
+    except KeyError:
+        raise ValueError('Do not understand file {p}')
+
+
+class _Json:
+    @staticmethod
+    def read(p):
         with p.open() as fp:
             return json.load(fp)
 
-    if p.suffix == '.toml':
+    @staticmethod
+    def write(data, p):
+        with safer.open(p, 'w') as fp:
+            json.dump(data, fp)
+
+
+class _Yaml:
+    @staticmethod
+    def read(p):
+        with p.open() as fp:
+            return _yaml().safe_load(fp)
+
+    @staticmethod
+    def write(data, p, **kwargs):
+        with safer.open(p, 'w') as fp:
+            _yaml().safe_dump(data, fp, **kwargs)
+
+
+def _yaml():
+    try:
+        import pyyaml
+        return pyyaml
+    except ImportError:
+        raise ImportError('Install `pyyaml` to handle .yaml files')
+
+
+class _Toml:
+    @staticmethod
+    def read(p):
         try:
             import tomllib as toml
         except ImportError:
             toml = _toml()
         return toml.loads(p.read_text())
 
-    if p.suffix == '.yaml':
-        return _yaml().safe_load(p.read_text())
-
-    if p.suffix in JSON_LINE_SUFFIXES:
-        return _read_jl(p)
-
-    raise ValueError(f'Cannot understand file {path}')
-
-
-def write(data: FileData, path: FilePath, **kwargs: Dict) -> None:
-    p = Path(path)
-
-    if p.suffix == '.json':
-        with safer.open(p, 'w') as fp:
-            json.dump(data, fp)
-
-    elif p.suffix == '.toml':
-        p.write_text(_toml().dumps(data))
-
-    if p.suffix == '.yaml':
-        return _yaml().safe_load(p.read_text())
-
-    if p.suffix in JSON_LINE_SUFFIXES:
-        return _read_jl(p)
-
-    raise ValueError(f'Cannot understand file {path}')
+    @staticmethod
+    def write(data, p, **kwargs):
+        p.write_text(_toml().dumps(data, **kwargs))
 
 
 def _toml():
@@ -62,15 +83,30 @@ def _toml():
         raise ImportError('Install `tomlkit` to handle .toml files')
 
 
-def _yaml()
-    try:
-        import pyyaml
-        return pyyaml
-    except ImportError:
-        raise ImportError('Install `pyyaml` to handle .yaml files')
+class _JsonLines:
+    @staticmethod
+    def read(p):
+        with p.open() as fp:
+            for line in fp:
+                yield json.loads(line)
+
+    @staticmethod
+    def write(data, p, *, use_safer=False, **kwargs):
+        if kwargs.get('indent') is not None:
+            raise ValueError('indent= not allowed for JSON Lines')
+
+        _open = safer.open if use_safer else open
+        with _open(p, 'w') as fp:
+            for d in data:
+                print(json.dumps(d), file=fp)
 
 
-def _read_jl(path):
-    with path.open() as fp:
-        for line in fp:
-            yield json.loads(line)
+SUFFIX_TO_CLASS = {
+    '.jl': _JsonLines,
+    '.jsonl': _JsonLines,
+    '.jsonline': _JsonLines,
+    '.yaml': _Yaml,
+    '.yml': _Yaml,
+    '.json': _Json,
+    '.toml': _Toml,
+}
